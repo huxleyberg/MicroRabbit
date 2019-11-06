3894470b7abf267c6a835f501179b334fadec6bc
chidi <huxleyberg@LIVE.COM>
2019-11-06 23:27:32 +0100

    implemented interfaces for the bus

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+using MicroRabbit.Domain.Core.Events;
+using MicroRabbit.Domain.Core.Commands;
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T comand) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
